c4f6a697decb351f40fa91e0d3d544d60a1ca7e4
parasomniya <parasomniya.m@gmail.com>
2026-04-10 22:07:00 +0700

    hw

--- hw02/fString.py
+++
@@ -1,4 +0,0 @@
-def format_number(num):
-    return f"{f'{num:,.3f}'.replace(',', ' ').replace('.', ' .'):*^30}"
-
-print(format_number(123488482390.28174))
--- hw02/unic.py
+++
@@ -1,8 +0,0 @@
-text = input()
-dct = {}
-for _ in text:
-    if _ in dct:
-        dct[_] += 1
-    else:
-        dct[_] = 1
-print(dct)
